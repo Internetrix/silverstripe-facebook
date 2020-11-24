0b93ad81b02dde9484c343c9ba2964e7cf2cf429
Iliain <drcv251@gmail.com>
2020-11-24 15:10:21 +1100

    Separated config check for reuse

--- src/Traits/FacebookHelperTrait.php
+++ src/Traits/FacebookHelperTrait.php
@@ -9,12 +9,23 @@ trait FacebookHelperTrait
 {
     public function createFacebookConnection()
     {
-        if (Config::inst()->exists('Internetrix\Facebook\Config', 'facebook_public_token') && Config::inst()->get('Internetrix\Facebook\Config', 'facebook_secret_token')) {
+        $config = $this->getFacebookConfig();
+
+        if ($config) {
             return new Facebook([
-                'app_id' => Config::inst()->get('Internetrix\Facebook\Config', 'facebook_public_token'),
-                'app_secret' => Config::inst()->get('Internetrix\Facebook\Config', 'facebook_secret_token'),
+                'app_id' => $config[0],
+                'app_secret' => $config[1],
                 'default_graph_version' => 'v8.0',
             ]);
+        } else {
+            return false;
+        }
+    }
+
+    public function getFacebookConfig()
+    {
+        if (Config::inst()->exists('Internetrix\Facebook\Config', 'facebook_public_token') && Config::inst()->exists('Internetrix\Facebook\Config', 'facebook_secret_token')) {
+            return [Config::inst()->get('Internetrix\Facebook\Config', 'facebook_public_token'), Config::inst()->get('Internetrix\Facebook\Config', 'facebook_secret_token')];
         } else {
             return false;
         }
